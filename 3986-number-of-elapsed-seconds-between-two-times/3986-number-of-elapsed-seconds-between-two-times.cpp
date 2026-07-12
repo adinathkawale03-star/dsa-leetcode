e626ed4b0f9e06c752b230354987cbde85c8dde1
adinathkawale03-star <adinathkawale03@gmail.com>
2026-07-12 11:45:06 +0530

    Time: 3 ms (18.31%), Space: 10.2 MB (20.38%) - LeetHub

--- 3986-number-of-elapsed-seconds-between-two-times/3986-number-of-elapsed-seconds-between-two-times.cpp
+++ 3986-number-of-elapsed-seconds-between-two-times/3986-number-of-elapsed-seconds-between-two-times.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int secondsBetweenTimes(string startTime, string endTime) {
-        int stthr=stoi(startTime.substr(0,2));
-        int sttmin=stoi(startTime.substr(3,2));
-        int sttss=stoi(startTime.substr(6,2));
-        int enthr=stoi(endTime.substr(0,2));
-        int entmin=stoi(endTime.substr(3,2));
-        int entss=stoi(endTime.substr(6,2));
-        int start=stthr*3600+sttmin*60+sttss;
-        int end=enthr*3600+entmin*60+entss;
-        return end-start;
+        int sthr=stoi(startTime.substr(0,2));
+        int stmn=stoi(startTime.substr(3,2));
+        int stss=stoi(startTime.substr(6,2));
+        int endhr =stoi(endTime.substr(0,2));
+        int endmn =stoi(endTime.substr(3,2));
+        int endss= stoi(endTime.substr(6,2));
+        
+        int startTm=sthr*3600 +stmn*60 +stss;
+        int endTm =endhr*3600 +endmn*60 +endss;
+        return endTm - startTm;
     }
 };
