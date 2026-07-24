cdbe47e9008def4a369927a9d9dad16d07ce1092
Adinath Kawale <adinathkawale03@gmail.com>
2026-07-24 23:51:38 +0530

    Time: 0 ms (100%), Space: 15 MB (96.86%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        //brute force problem solution 
-        int n=nums.size();
-        int ans=-1;
-        for(int i=0;i<n;i++){
-            if(nums[i]==target){
-                ans=i;
-                break;
+        //optimal aproch problem solution 
+        int low = 0, high = nums.size() - 1; 
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
             }
         }
-        return ans;
+        return -1; 
     }
 };
