6d2abd7c7cdba3aacf3abfa1b8bfbeec99d02a36
Adinath Kawale <adinathkawale03@gmail.com>
2026-07-28 16:09:35 +0530

    Time: 8 ms (54.51%), Space: 22.9 MB (84.02%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,33 +1,30 @@
 class Solution {
-private:
-    int findmax(vector<int> piles){
-        int maxi=INT_MIN;
-        for(int I:piles){
-            maxi=max(I,maxi);
-        }
-        return maxi;
-    }
-    long long calculatehre(vector<int> piles,int hours){
-        long long totalh=0;
-        for(int i:piles){
-            totalh+=ceil((double)i/(double)hours);
-        }
-        return totalh;
-    }
 public:
+    bool canEat(vector<int>& piles, int h, int k) {
+        long long hours = 0;
+
+        for (int pile : piles) {
+            hours += (pile + k - 1) / k;
+        }
+
+        return hours <= h;
+    }
+
     int minEatingSpeed(vector<int>& piles, int h) {
-      int low=1;
-      int high=findmax(piles);
-      while(low<=high){
-        int mid=low+(high-low)/2;
-        long long totalhrs=calculatehre(piles,mid);
-        if(totalhrs<=h){
-            high=mid-1;
+
+        int low = 1;
+        int high = *max_element(piles.begin(), piles.end());
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (canEat(piles, h, mid))
+                high = mid - 1;   
+            else
+                low = mid + 1;    
         }
-        else{
-            low=mid+1;
-        }
-      }
-      return low;
+
+        return low;
     }
 };
