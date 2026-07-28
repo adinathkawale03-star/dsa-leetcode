5d7a9c7caf552019691b82c2024f67e941b1c10b
Adinath Kawale <adinathkawale03@gmail.com>
2026-07-28 17:12:51 +0530

    Time: 0 ms (100%), Space: 8.1 MB (41.37%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.length()!=goal.length()){return false;}
+        string a=s+s;
+        return a.find(goal)!=string::npos;
+   }
+};
