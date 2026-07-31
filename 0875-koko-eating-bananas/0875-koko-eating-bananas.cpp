1f0b2f18e58199a9af58b05a7925c3e6eb650340
Adinath Kawale <adinathkawale03@gmail.com>
2026-07-31 17:50:37 +0530

    Time: 26 ms (5.05%), Space: 22.9 MB (52.84%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,27 +1,33 @@
 class Solution {
-public:
-    bool check(vector<int>& piles,int mid ,int h){
-        long long hours=0;
-        for(int pile:piles){
-            hours=hours+(pile+mid-1)/mid;
-        }
-        return hours<=h;
+private: int maxi(vector<int>& nums){
+    int maxi=INT_MIN;
+    for(int i:nums){
+        maxi=max(i,maxi);
     }
+    return maxi;
+}
+    long long check(vector<int>& nums,long long mid){
+        long long hrs=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            hrs+=ceil((double)nums[i]/(double)mid);
+        }
+        return hrs;
+    }
+public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int low=1;
-        int high=*max_element(piles.begin(),piles.end());
+        int high=maxi(piles);
         while(low<=high){
-            int mid=low+(high-low)/2;
-            if(check(piles,mid,h)){
-                high=mid-1;
+            long long mid=low+((high-low)/2);
+            long long help=check(piles,mid);
+            if(help<=h){
+                high=mid-1;//shifting to the smaller value then these value
             }
             else{
-                low=mid+1;
+                low=mid+1;//shifting to the upper value because they required more time so for less upper value
             }
         }
         return low;
-    
-    
-    
     }
 };
