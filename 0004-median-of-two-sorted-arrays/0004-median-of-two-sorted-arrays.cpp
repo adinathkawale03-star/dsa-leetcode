32e932f8f47fde9ec7cefd1a54cba7b24657d5e8
Adinath Kawale <adinathkawale03@gmail.com>
2026-08-01 17:21:30 +0530

    Time: 0 ms (100%), Space: 95.2 MB (42.96%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,30 +1,61 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        //brute force approch for these problem 
+        //better approch for these problem 
         int n1=nums1.size();
         int n2=nums2.size();
-        vector<int> nums3;
-        int i=0;
-        int j=0;
+        int n=n1+n2;
+        int ind2=n/2;
+        int ind1=ind2-1;
+        int ind1ele=-1;
+        int ind2ele=-1;
+        int i=0,j=0,cnt=0;
         while(i<n1 && j<n2){
             if(nums1[i]>nums2[j]){
-                nums3.push_back(nums2[j++]);
+                if(cnt==ind1){
+                    ind1ele=nums2[j];
+                }
+                if(cnt==ind2){
+                    ind2ele=nums2[j];
+                }
+                cnt++;
+                j++;
             }
             else{
-                nums3.push_back(nums1[i++]);
+                if(cnt==ind1){
+                    ind1ele=nums1[i];
+                }
+                if(cnt==ind2){
+                    ind2ele=nums1[i];
+                }
+                cnt++;
+                i++;
             }
         }
         while(i<n1){
-            nums3.push_back(nums1[i++]);
+             if(cnt==ind1){
+                  ind1ele=nums1[i];
+             }
+             if(cnt==ind2){
+                  ind2ele=nums1[i];
+             }
+             i++;
+             cnt++;
         }
         while(j<n2){
-            nums3.push_back(nums2[j++]);
+            if(cnt==ind1){
+                ind1ele=nums2[j];
+            }
+            if(cnt==ind2){
+                ind2ele=nums2[j];
+            }
+            j++;
+            cnt++;
         }
-        int n=n1+n2;
-        if(n%2==0){
-            return (double)(((double)nums3[n/2]+(double)nums3[n/2-1])/2);
+        if(n%2==1){
+            return (double)ind2ele;
         }
-        return (double)nums3[n/2];
+        return (double)(((double)ind1ele+(double)ind2ele)/2);
+      
     }
 };
