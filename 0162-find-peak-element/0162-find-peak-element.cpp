d38d4130b1afab2a13894cbc6d32314272c571dd
Adinath Kawale <adinathkawale03@gmail.com>
2026-08-04 14:33:34 +0530

    Time: 0 ms (100%), Space: 12.7 MB (6.48%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,11 +1,27 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        //linear search
+        //binary search
         int n=nums.size();
-        for(int i=0;i<n;i++){
-            if((i==0 || nums[i-1]<nums[i]) && (i==n-1 || nums[i+1]<nums[i])){
-                return i;
+        if(n==1){return 0;}
+        if(nums[0]>nums[1]){
+            return 0;
+        }
+        if(nums[n-1]>nums[n-2]){
+            return n-1;
+        }
+        int low=1;
+        int high=n-2;
+        while(low<=high){
+            int mid=low+((high-low)/2);
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+                return mid;
+            }
+            if(nums[mid-1]<nums[mid] && nums[mid]<nums[mid+1]){
+                low=mid+1;//element in the ascending order and in the further array peak will be found
+            }
+            else{
+                high=mid-1;//element i descending order in the befor mid peak will be found
             }
         }
         return n;
