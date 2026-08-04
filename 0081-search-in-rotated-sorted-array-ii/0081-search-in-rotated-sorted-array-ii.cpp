dc5cf16d3eb8522a9b0ba9c7ed692563cd29dc08
Adinath Kawale <adinathkawale03@gmail.com>
2026-08-04 14:07:08 +0530

    Time: 0 ms (100%), Space: 18 MB (46.77%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,12 +1,37 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        //brute approch for these problem
-        for(int i:nums){
-            if(i==target){
+        //the difference between the firt and the second problem are the in these problem dupilcate will be present 
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid=low+((high-low)/2);
+            if(nums[mid]==target){
                 return true;
+            }
+            else if(nums[low]==nums[mid] && nums[mid]==nums[high]){
+                high--;
+                low++;
+            }
+            else if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && target<=nums[mid]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<=target && target<=nums[high]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
             }
         }
         return false;
+    
     }
 };
