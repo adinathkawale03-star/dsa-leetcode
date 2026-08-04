cb85b2c19003c360eefd1c503d6fb715642f86f5
Adinath Kawale <adinathkawale03@gmail.com>
2026-08-04 14:02:39 +0530

    Time: 0 ms (100%), Space: 15.2 MB (36.81%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n=nums.size();
-        int low=0;
-        int high=n-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]==target){return mid;}
-            else if(nums[low]<=nums[mid]){
-                if(nums[low]<=target && target<=nums[mid]){
-                    high=mid-1;
-                }
-                else{
-                    low=mid+1;
-                }
+       //in these problem we can use the binary search and values in that are the distinct (not the same)
+       int n=nums.size();
+       int low=0;
+       int high=n-1;
+       while(low<=high){
+        int mid=low+((high-low)/2);
+        if(nums[mid]==target){
+            return mid;
+        }
+        else if(nums[low]<=nums[mid]){
+            if(nums[low]<=target && target<=nums[mid]){
+                high=mid-1;
             }
             else{
-                if(nums[mid]<=target && target<=nums[high]){
-                    low=mid+1;
-                }
-                else{
-                    high=mid-1;
-                }
+                low=mid+1;
             }
         }
-        return -1;
+        else{
+            if(nums[mid]<=target && target<=nums[high]){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+       }
+       return -1;
     }
 };
