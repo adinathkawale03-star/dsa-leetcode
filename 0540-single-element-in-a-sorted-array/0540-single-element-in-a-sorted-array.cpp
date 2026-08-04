e5e748d4da2ff1964348ab721380980a8ddf6653
Adinath Kawale <adinathkawale03@gmail.com>
2026-08-04 14:26:08 +0530

    Time: 0 ms (100%), Space: 26.1 MB (94.29%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,11 +1,29 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        //brute force solution 
-        int xr=0;
-        for(int i:nums){
-            xr=xr^i;
+        //binary search in these problem approch are the  sequence in the order if the double element are arranged from the begining we should have the 
+        int n=nums.size();
+        int low=1;
+        int high=n-2;
+        if(n==1){return nums[0];}
+        if(nums[0]!=nums[1]){
+            return nums[0];
         }
-        return xr;
+        if(nums[n-1]!=nums[n-2]){
+            return nums[n-1];
+        }
+        while(low<=high){
+            int mid=low+((high-low)/2);
+            if(nums[mid]!=nums[mid-1]  && nums[mid+1]!=nums[mid]){
+                return nums[mid];
+            }
+            if((mid%2==0 && nums[mid]==nums[mid+1] )|| (mid%2==1 && nums[mid]==nums[mid-1])){//in these condition to the mid no single element appeared
+                low=mid+1;  
+            }
+            else{
+                high=mid-1;//single element appeared in the mid element in the array
+            }
+        }
+        return -1;
     }
 };
