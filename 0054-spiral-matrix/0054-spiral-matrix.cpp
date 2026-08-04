105c0408f22fbe871aecddc4f40f9983c3e46f30
Adinath Kawale <adinathkawale03@gmail.com>
2026-08-04 16:22:33 +0530

    Time: 0 ms (100%), Space: 9.3 MB (57.19%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> ans;
-        int n=matrix.size();
-        int m=matrix[0].size();
-        int left=0;
-        int right=m-1;
-        int top=0;
-        int bottom=n-1;
-        while(left<=right && top<=bottom){
-            for(int i=left;i<=right;i++){
-                ans.push_back(matrix[top][i]);
-            }
-            top++;
-            for(int j=top;j<=bottom;j++){
-                ans.push_back(matrix[j][right]);
-            }
-            right--;
-            if(top<=bottom){
-                for(int i=right;i>=left;i--){
-                    ans.push_back(matrix[bottom][i]);
-                }
-                bottom--;
-            }
-            if(left<=right){
-                for(int i=bottom;i>=top;i--){
-                    ans.push_back(matrix[i][left]);
-                }
-                left++;
-            }
+       vector<int> temp;
+       int n=matrix.size();
+       int m=matrix[0].size();
+       int top=0;
+       int left=0;
+       int bottom=n-1;
+       int right=m-1;
+       while(left<=right && top<=bottom){
+        for(int i=left;i<=right;i++){
+            temp.push_back(matrix[top][i]);
         }
-        return ans;
+        top++;
+        for(int i=top;i<=bottom;i++){
+            temp.push_back(matrix[i][right]);
+        }
+        right--;
+        if(top<=bottom){
+            for(int i=right;i>=left;i--){
+                temp.push_back(matrix[bottom][i]);
+            }
+            bottom--;
+        }
+        if(left<=right){
+            for(int i=bottom;i>=top;i--){
+                temp.push_back(matrix[i][left]);
+            }
+            left++;
+        }
+       }
+       return temp;
     }
 };
