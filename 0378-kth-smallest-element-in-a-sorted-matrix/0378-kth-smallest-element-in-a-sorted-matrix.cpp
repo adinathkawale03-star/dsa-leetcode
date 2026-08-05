a9e9bfc9bbce05f73bf50aad89bf8ac0d7d681f1
Adinath Kawale <adinathkawale03@gmail.com>
2026-08-05 19:05:05 +0530

    Time: 0 ms (100%), Space: 16.9 MB (95.71%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,15 +1,39 @@
 class Solution {
-public:
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
-        //brute force solution for these problem
-        int n=matrix.size(); 
-        vector<int> temp;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                temp.push_back(matrix[i][j]);
+private:
+    int check(vector<vector<int>>& nums,int mid){
+        int i=0;
+        int n=nums.size();
+        int j=n-1;
+        int count=0;
+        while(i>=0 && j>=0 && i<n && j<n){
+            if(nums[i][j]<=mid){
+                count+=(j+1);
+                i++;
+            }
+            else{
+                j--;
             }
         }
-        sort(temp.begin(),temp.end());
-        return temp[k-1];
+        return count;
+    }
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        //optimal solution for these problem are the use the technique to find the element in the 2d matrix present or the not 
+        int n=matrix.size();
+        int low=matrix[0][0];
+        int high=matrix[n-1][n-1];
+        int ans=-1;
+        while(low<=high){
+            int mid=low+((high-low)/2);
+            int help=check(matrix,mid);
+            if(help>=k){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
     }
 };
