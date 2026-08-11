a7d8358593b7daf3aa28590c890285581c37ba37
Adinath Kawale <adinathkawale03@gmail.com>
2026-08-11 08:56:34 +0530

    Time: 0 ms (100%), Space: 54.4 MB (82.56%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        //brute force solution 
+        int n1=nums1.size();
+        int n2=nums2.size();
+        int i=0;
+        int j=0;
+        while(i<n1 && j<n2){
+            if(nums1[i]==nums2[j]){
+                return nums1[i];
+            }
+            if(nums1[i]>nums2[j]){
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+        return -1;
+    }
+};
