c9477c65b2d283553d05367c7fe8fa8ca3e547bd
Adinath Kawale <adinathkawale03@gmail.com>
2026-08-15 19:40:40 +0530

    Time: 3 ms (18.16%), Space: 9 MB (57.98%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-      //brute force solution for these 
-      int carry=0;
-      int n=a.size()-1;
-      int m=b.size()-1;
-      string ans;
-      while(n>=0 || m>=0 || carry!=0){
-        int sum=(((n+1)?(a[n]-'0'):0)+((m+1)?(b[m]-'0'):0)+carry);
-        if(sum==0 || sum==1){
-            carry=0;
-            ans.push_back(sum+'0');
+        string ans;
+
+        ans.reserve(max(a.size(), b.size()) + 1);
+
+        int i = a.size() - 1;
+        int j = b.size() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+            if (i >= 0) sum += a[i--] - '0';
+            if (j >= 0) sum += b[j--] - '0';
+
+            carry = sum / 2;
+            ans.push_back((sum % 2) + '0');
         }
-        else if(sum==2 || sum==3){
-            carry=1;
-            if(sum==2){
-                ans.push_back('0');
-            }
-            else{
-                ans.push_back('1');
-            }
-        }
-        if(n+1){n--;}
-        if(m+1){m--;}
-      }
-      reverse(ans.begin(),ans.end());
-      return ans;
+
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
