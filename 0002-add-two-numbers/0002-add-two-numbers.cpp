27042453676f5b1bcf6fac71d4697727066bb644
Adinath Kawale <adinathkawale03@gmail.com>
2026-08-15 19:03:56 +0530

    Time: 2 ms (39.08%), Space: 77.3 MB (13.14%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,30 +11,21 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-       //striver solution optimal only one
        ListNode* dummy=new ListNode(-10);
        ListNode* curr=dummy;
        int carry=0;
        while(l1!=NULL || l2!=NULL){
-        int sum=carry;
-        if(l1){sum+=l1->val;}
-        if(l2){sum+=l2->val;}
-        ListNode* newnode=new ListNode(sum%10);
-        curr->next=newnode;
-        curr=curr->next;
-        if(l1){
-            l1=l1->next;
-        }
-        if(l2){
-            l2=l2->next;
-        }
-        sum=sum/10;
-        carry=sum;
+          int sum=((l1)?l1->val:0)+((l2)?l2->val:0)+carry;
+          ListNode* newnode=new ListNode(sum%10);
+          curr->next=newnode;
+          curr=curr->next;
+          if(l1){l1=l1->next;}
+          if(l2){l2=l2->next;}
+          carry=sum/10;
        }
-       if(carry){
-        ListNode* newnode=new ListNode(carry);
-        curr->next=newnode;
-        curr=curr->next;
+       if(carry){ListNode* newnode=new ListNode(carry);
+          curr->next=newnode;
+          curr=curr->next;
        }
        ListNode* head=dummy->next;
        return head;
