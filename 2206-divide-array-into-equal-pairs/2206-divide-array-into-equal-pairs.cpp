9b11cc990f8945469ec6e161745372fac9ac9f38
Adinath Kawale <adinathkawale03@gmail.com>
2026-08-24 22:03:30 +0530

    Time: 0 ms (100%), Space: 17.3 MB (68.62%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.cpp
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     bool divideArray(vector<int>& nums) {
-        unordered_map<int,int> a;
+        int freq[501];
         for(int i:nums){
-            a[i]++;
+            freq[i]++;
         }
-        for(auto it:a){
-            if(it.second%2==1){
+        for(int i=1;i<=500;i++){
+            if(freq[i]%2==1){
                 return false;
             }
         }
         return true;
-        //tc:o(n):nearly , sc: o(n)
+        //tc:o(n):nearly , sc: o(1) nearly becuase the 501 is very small number 
     }
 };
