20201f44698c3bf729ff8e9b86346333d135efec
Adinath Kawale <adinathkawale03@gmail.com>
2026-08-28 18:20:35 +0530

    Time: 0 ms (100%), Space: 26.3 MB (37.07%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int n = nums.size();
-        int duplicate = 0, missing = 0;
-        
-        sort(nums.begin(), nums.end());
-
-        for(int i = 0; i < n - 1; i++){
-            if(nums[i] == nums[i+1]){ 
-                duplicate = nums[i]; 
+        int repeateadly;
+        int n=nums.size();
+        vector<int> arr(n+1,0);
+        for(int i:nums){
+            arr[i]++;
+            if(arr[i]>1){
+                repeateadly=i;
             }
         }
-
-        int expectedSum = n * (n + 1) / 2;
-        int actualSum = 0;
-        
-        for(int i = 0; i < n; i++) {
-            actualSum += nums[i];
+        for(int i=1;i<=n;i++){
+            if(arr[i]==0){
+                return {repeateadly,i};
+            }
         }
-        
-        missing = expectedSum - (actualSum - duplicate);
+        return {0,0};
 
-        return {duplicate, missing};
     }
 };
