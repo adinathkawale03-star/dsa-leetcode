47b87d2f5365f5ffa7d2153290852feaa40926dc
Adinath Kawale <adinathkawale03@gmail.com>
2026-09-05 19:13:26 +0530

    Time: 0 ms (100%), Space: 11.6 MB (6.11%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.c
+++ 0206-reverse-linked-list/0206-reverse-linked-list.c
@@ -1,20 +1,17 @@
 struct ListNode* reverseList(struct ListNode* head) {
-    if(!head || !head->next){
-        return head;
-    }
-    struct ListNode* ans;
-    ans=malloc(sizeof(struct ListNode));
-    struct ListNode* temp=head;
-    ans->val=temp->val;
-    temp=temp->next;
-    ans->next=NULL;
-    while(temp!=NULL){
-        struct ListNode* newnode;
-        newnode=malloc(sizeof(struct ListNode));
-        newnode->val=temp->val;
-        newnode->next=ans;
-        ans=newnode;
-        temp=temp->next;
-    }
-    return ans;
+   if(!head || !head->next){return head;}
+   struct ListNode* temp=malloc(sizeof(struct ListNode));
+   temp->val=head->val;
+   temp->next=NULL;
+   head=head->next;
+   struct ListNode* temp2=temp;
+   struct ListNode* temp3=head;
+   while(temp3!=NULL){
+    struct ListNode* newnode=malloc(sizeof(struct ListNode));
+    newnode->val=temp3->val;
+    newnode->next=temp;
+    temp=newnode;
+    temp3=temp3->next;
+   }
+   return temp;
 }
