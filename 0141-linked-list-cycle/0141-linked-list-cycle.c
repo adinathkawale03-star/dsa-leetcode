2367805152b05d2e6a229e47583ec5b6bb3dc52f
Adinath Kawale <adinathkawale03@gmail.com>
2026-09-05 18:41:07 +0530

    Time: 7 ms (91.98%), Space: 11.9 MB (85.24%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.c
+++ 0141-linked-list-cycle/0141-linked-list-cycle.c
@@ -6,18 +6,15 @@
  * };
  */
 bool hasCycle(struct ListNode *head) {
-    if(!head || !head->next){
-        return false;
+   struct ListNode* slow=head;
+   struct ListNode* fast=head;
+   while(fast!=NULL && fast->next!=NULL){
+    slow=slow->next;
+    fast=fast->next->next;
+    if(slow==fast){
+        return true;
     }
-    struct ListNode *slow=head;
-    struct ListNode *fast=head;
-    while(fast!=NULL && fast->next!=NULL){
-        slow=slow->next;
-        fast=fast->next->next;
-        if(slow==fast){
-            return true;
-        }
-    }
-    return false;
+   }
+   return false;
 
 }
