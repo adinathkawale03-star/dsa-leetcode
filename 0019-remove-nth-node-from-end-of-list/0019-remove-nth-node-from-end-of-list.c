8ee1898980473dc486a149d427d777c5506e303f
Adinath Kawale <adinathkawale03@gmail.com>
2026-09-06 11:35:03 +0530

    Time: 0 ms (100%), Space: 10.1 MB (96.88%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.c
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.c
@@ -6,29 +6,24 @@
  * };
  */
 struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
-    if(!head || !head->next){
-        return NULL;
-    }
-    int len=0;
+    if(!head || !head->next){return NULL;}
+    int l=0;
     struct ListNode* temp=head;
     while(temp!=NULL){
-        len++;
+        l++;
         temp=temp->next;
     }
-    int a=len-n;
-    if(a==0){
-        return head->next;
-    }
+    l=l-n;
+    n=0;
     temp=head;
-    int cnt=0;
+    if(l==0){return head->next;}
     while(temp!=NULL){
-        cnt++;
-        if(cnt==a){
+        n++;
+        if(l==n){
             temp->next=temp->next->next;
             return head;
         }
         temp=temp->next;
     }
     return head;
-
 }
