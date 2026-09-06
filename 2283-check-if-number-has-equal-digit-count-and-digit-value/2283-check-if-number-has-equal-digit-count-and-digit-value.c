c10f249df50c29fe369a63b4cde533265ef07a63
Adinath Kawale <adinathkawale03@gmail.com>
2026-09-06 14:24:18 +0530

    Time: 0 ms (100%), Space: 8.4 MB (82.05%) - LeetHub

---
+++ 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <stdbool.h>
+
+bool digitCount(char* num) {
+    int n = strlen(num);
+    int a[10] = {0};
+    for (int i = 0; i < n; i++) {
+        a[num[i] - '0']++;
+    }
+    for (int i = 0; i < n; i++) {
+        if (a[i] != (num[i] - '0')) {
+            return false;
+        }
+    }
+    return true;
+}
