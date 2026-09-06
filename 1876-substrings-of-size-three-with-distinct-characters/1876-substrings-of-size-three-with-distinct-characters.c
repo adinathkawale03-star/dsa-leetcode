9ced972d5054502bcbfdb62105b28547e6da3662
Adinath Kawale <adinathkawale03@gmail.com>
2026-09-06 14:27:40 +0530

    Time: 0 ms (100%), Space: 8.6 MB (45.63%) - LeetHub

---
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.c
@@ -0,0 +1,10 @@
+int countGoodSubstrings(char* s) {
+    if(strlen(s)<3){return 0;}
+   int ans=0;
+   for(int i=0;i<strlen(s)-2;i++){
+    if(s[i]!=s[i+1] && s[i+1]!=s[i+2] && s[i]!=s[i+2]){
+        ans++;
+    }
+   } 
+   return ans;
+}
