956802af29f171ce86a588d374d5112e6f2a795d
Adinath Kawale <adinathkawale03@gmail.com>
2026-09-11 12:02:58 +0530

    Time: 2 ms (72.71%), Space: 14.9 MB (44.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-       unordered_map<int,int> a;
-       for(int i=0;i<nums.size();i++){
-        int num=target-nums[i];
-        if(a.find(num)!=a.end()){
-            return {a[num],i};
+    //striver sde sheet revision
+        unordered_map<int,int> a;
+        for(int i=0;i<nums.size();i++){
+            int num=target-nums[i];
+            if(a.find(num)!=a.end()){
+              return {a[num],i};
+            }
+            a[nums[i]]=i;
         }
-        a[nums[i]]=i;
-       }
-       return {-1,-1};
+        return {-1,-1};
     }
 };
